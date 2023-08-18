6f4a1959a51ba7c8e185d347e119917b35146ecc
Rafael de Luca <deluca1712@gmail.com>
2023-08-18 16:52:29 -0300

    redirect with component Navigate when route not found

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import ProductDetails from "./routes/ClientHome/ProductDetails";
 import ClientHome from "./routes/ClientHome";
 import ProductCatalog from "./routes/ClientHome/ProductCatalog";
 
 function App() {
+
+
 
   return (
     <BrowserRouter>
@@ -13,6 +15,8 @@ function App() {
           <Route path="product-catalog" element={<ProductCatalog />}> </Route>
           <Route path="product-details/:productId" element={<ProductDetails />}></Route>
         </Route >
+        {/*elemento navite para redirecionar*/}
+        <Route path="*" element={<Navigate to="/" />}> </Route>
       </Routes >
     </BrowserRouter >
   );
