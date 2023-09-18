41eb23bdc2e306372ef8941c6afce4ab6338c8d0
Rafael de Luca <deluca1712@gmail.com>
2023-09-18 15:50:12 -0300

    Kart with mock data

--- src/routes/ClientHome/Cart/index.tsx
+++ src/routes/ClientHome/Cart/index.tsx
@@ -1,50 +1,67 @@
 import './styles.css';
 
-import computerImg from '../../../assets/computer.png';
-import tableImg from '../../../assets/tablet.png';
 import ButtonBlue from '../../../components/ButtonBlue';
 import ButtonWhite from '../../../components/ButtonWhite';
 
+
+const cartMockData = {
+    items: [
+        {
+            productId: 1,
+            quantity: 1,
+            name: "The Lord of the Rings",
+            price: 90.5,
+            imgUrl: "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/1-big.jpg"
+        },
+        {
+            productId: 2,
+            quantity: 2,
+            name: "Smart TV",
+            price: 2190.0,
+            imgUrl: "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg"
+        },
+    ]
+}
+
 export default function Kart() {
+
+    const totalCart = () => {
+        let sum = 0.0;
+        cartMockData.items.map(item => (
+            sum += (item.quantity * item.price)
+        ))
+        return sum.toFixed(2);
+    }
 
     return (
         <main>
             <section id="cart-container-section" className="ec-container">
                 <div className="ec-card-general ec-margin-bottom-20px">
-                    <div className="ec-cart-item-container ec-line-bottom">
-                        <div className="ec-cart-item-left">
-                            <img src={tableImg} alt="Tablet" />
-                            <div className="ec-cart-item-description">
-                                <h3>Tablet Sansung core i5</h3>
-                                <div className="ec-cart-item-quantity-container">
-                                    <div className="ec-cart-item-quantity-btn">-</div>
-                                    <p>2</p>
-                                    <div className="ec-cart-item-quantity-btn">+</div>
+
+                    {
+                        cartMockData.items.map(item => (
+                            <div key={item.productId} className="ec-cart-item-container ec-line-bottom">
+                                <div className="ec-cart-item-left">
+                                    <img src={item.imgUrl} alt={item.name} />
+                                    <div className="ec-cart-item-description">
+                                        <h3>{item.name}</h3>
+                                        <div className="ec-cart-item-quantity-container">
+                                            <div className="ec-cart-item-quantity-btn">-</div>
+                                            <p>{item.quantity}</p>
+                                            <div className="ec-cart-item-quantity-btn">+</div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="ec-cart-item-right">
+                                    R$ {(item.price * item.quantity).toFixed(2)}
                                 </div>
                             </div>
-                        </div>
-                        <div className="ec-cart-item-right">
-                            R$ 4000,00
-                        </div>
-                    </div>
-                    <div className="ec-cart-item-container ec-line-bottom">
-                        <div className="ec-cart-item-left">
-                            <img src={computerImg} alt="Computador" />
-                            <div className="ec-cart-item-description">
-                                <h3>Computador Gamer Professional</h3>
-                                <div className="ec-cart-item-quantity-container">
-                                    <div className="ec-cart-item-quantity-btn">-</div>
-                                    <p>2</p>
-                                    <div className="ec-cart-item-quantity-btn">+</div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="ec-cart-item-right">
-                            R$ 5000,00
-                        </div>
-                    </div>
+                        ))
+                    }
+
+
                     <div className="ec-cart-total-container">
-                        <h3>R$ 18000,00</h3>
+                        <h3>R$ {totalCart()}</h3>
                     </div>
                 </div>
                 <div className="ec-btn-container">
