9a680ec17989da62050fcbfa0383b9f21e6c0953
Rafael de Luca <deluca1712@gmail.com>
2023-09-27 14:13:09 -0300

    Create complex object QueryParameters

--- src/routes/ClientHome/ProductCatalog/index.tsx
+++ src/routes/ClientHome/ProductCatalog/index.tsx
@@ -6,22 +6,33 @@ import * as productService from "../../../services/product-services";
 import { ProductDTO } from '../../../models/product';
 import { useEffect, useState } from 'react';
 
- /*
-    const objectTest : CategoryDTO = {
-        id: 15,
-        name: "Tablet"
-    }
-    */
+/*
+   const objectTest : CategoryDTO = {
+       id: 15,
+       name: "Tablet"
+   }
+   */
+
+// quando for mais de um parametro que filtrar a busca, usar um objeto composto
+// não criar um useState para cada parâmetro
+type QueryParameters = {
+    page: number;
+    name: string;
+}
 
 const ProductCatalog = () => {
-   
+
     // useState para armazenas o estado dentro do component
     // retorna um list de produtos do backend , valor inicial é uma empty list
-    const [products, setProducts] = useState<ProductDTO []>([]);
+    const [products, setProducts] = useState<ProductDTO[]>([]);
 
-    const [productName, setProductName] = useState<string>("");
+    //const [productName, setProductName] = useState<string>("");
+    const [queryParameters, setQueryparameters] = useState<QueryParameters>({
+        page: 0,
+        name: ""
+    });
 
-    useEffect ( ()=> {
+    useEffect(() => {
 
         // JSON.stringify -> converte de objeto para string
         // JSON.parse     ->  convrtet de string para objeto
@@ -38,14 +49,17 @@ const ProductCatalog = () => {
 
         // requisição para quando montar o componet do catalogo
         // parametro incial é pagina 0 e sem informar o name do produto string vazia
-        productService.findAllPageRequest(0, productName)
+        productService.findAllPageRequest(queryParameters.page, queryParameters.name)
             .then((requestPromiseResponse) => {
                 setProducts(requestPromiseResponse.data.content);
             });
-    },[productName]) // lista de dependências do use effect
+    }, [queryParameters]) // lista de dependências do use effect
 
-    const handleSearch = (searchBarText : string) => {
-        setProductName(searchBarText);
+    const handleSearch = (searchBarText: string) => {
+        setQueryparameters({
+            ...queryParameters,
+            name: searchBarText
+        });
     }
 
     return (
@@ -55,10 +69,10 @@ const ProductCatalog = () => {
                     <SearchBar eventOnSearch={handleSearch}></SearchBar>
                     <div className="ec-catalog-cards ec-margin-top-20px ec-margin-bottom-20px">
                         {
-                            
+
                             products.map(
                                 productBackend =>
-                                <ProductCatalogCard key={productBackend.id} product={productBackend}></ProductCatalogCard>
+                                    <ProductCatalogCard key={productBackend.id} product={productBackend}></ProductCatalogCard>
                             )
                         }
                     </div>
