86ab442a5a35c46a5919c740875f38e099ce0f62
Rafael de Luca <deluca1712@gmail.com>
2023-10-02 15:56:45 -0300

    getting number of cart items from localStorage

--- src/components/CartIcon/index.tsx
+++ src/components/CartIcon/index.tsx
@@ -1,11 +1,16 @@
 import cartImg from '../../assets/cart.svg';
 import './styles.css';
+import * as cartService from '../../services/cart-service';
+import { useState } from 'react';
+
 const CartIcon = () => {
+
+    const [cart, setCart] = useState(cartService.getCart());
 
     return (
         <>
             <img src={cartImg} alt="Carrinho de compras" />
-            <div className="ec-cart-number">3</div>
+            <div className="ec-cart-number">{cart.items.length}</div>
         </>
     );
 }
