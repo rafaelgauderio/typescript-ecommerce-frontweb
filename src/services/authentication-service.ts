f08ccb136d61ad15cb44edfb0afc3e7314202e0b
Rafael de Luca <deluca1712@gmail.com>
2023-10-06 16:35:27 -0300

    Method save, get and delete token from localStorage

--- src/services/authentication-service.ts
+++ src/services/authentication-service.ts
@@ -1,12 +1,13 @@
 import QueryString from "qs";
 import { CredentialsDTO } from "../models/authentication";
 import { CLIENT_ID, CLIENT_SECRET } from "../utils/system";
-import { AxiosRequestConfig } from "axios";
+import { AxiosPromise, AxiosRequestConfig } from "axios";
 import { requestBackend } from "./request";
+import * as accessTokenRepository from '../localStorage/access-token-repository';
 
 const encryptedString = window.btoa(CLIENT_ID + ":" + CLIENT_SECRET);
 
-export function loginRequest(loginData: CredentialsDTO) {
+export function loginRequest(loginData: CredentialsDTO) : AxiosPromise {
     // cabeçalho da requisição
     const requestHeaders = {
         "Content-Type": "application/x-www-form-urlencoded",
@@ -34,5 +35,19 @@ export function loginRequest(loginData: CredentialsDTO) {
     //console.log(requestHeaders);
     //console.log(loginData);
     //console.log(requestBody);
-
 }
+
+export function saveAccessToken(token : string) : void {
+    accessTokenRepository.saveToken(token);
+}
+
+export function getAccessToken() : void {
+    accessTokenRepository.getToken();
+}
+
+// se o usuário não tiver um token salvo no localStorage
+// ele não consegue fazer requisições a recursos protegidos (não está logado)
+export function logout() : void{
+    accessTokenRepository.removeToken();
+}
+
