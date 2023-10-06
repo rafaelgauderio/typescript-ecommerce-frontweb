1762398d89a8b648730b461e8fabab7f500c921f
Rafael de Luca <deluca1712@gmail.com>
2023-10-06 16:23:02 -0300

    auxiliar component AccessTokenRepository

--- src/utils/system.ts
+++ src/utils/system.ts
@@ -1,6 +1,7 @@
 export const BASE_URL = "http://localhost:8080";
 
 export const CART_KEY = "com.rafaeldeluca.ecommerce/cart";
+export const TOKEN_KEY = "com.rafaeldeluca.ecommerce/token";
 
 export const CLIENT_ID = "myclientid";
 
