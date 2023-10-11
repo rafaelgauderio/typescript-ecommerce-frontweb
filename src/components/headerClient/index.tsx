a6a440416a117a15805b85b8391f88459c17f5a2
Rafael de Luca <deluca1712@gmail.com>
2023-10-11 11:58:09 -0300

    Conditional rendering of admin icon

--- src/components/headerClient/index.tsx
+++ src/components/headerClient/index.tsx
@@ -1,6 +1,9 @@
 import { Link } from 'react-router-dom';
 import './styles.css';
 import CartIcon from '../CartIcon';
+import adminIcon from '../../assets/admin.svg';
+import * as authenticationService from '../../services/authentication-service';
+
 
 const HeaderClient = () => {
 
@@ -12,6 +15,16 @@ const HeaderClient = () => {
                 </Link>
                 <div className="ec-navbar-header-right">
                     <div className="ec-menu-items-container">
+
+                        {/*Só renderiza o componente admin icon se estiver logado como admin*/}
+                        {
+                            authenticationService.userHasAnyRoles(['ROLE_ADMIN']) &&
+                            <Link to="/admin">
+                                <div className="ec-menu-item">
+                                    <img src={adminIcon} alt="Admin"></img>
+                                </div>
+                            </Link>
+                        }
                         <Link to='/cart'>
                             <div className="ec-menu-item">
                                 <CartIcon></CartIcon>
