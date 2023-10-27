7106d971dc681a215667a8684fce318665d7a7e5
Rafael de Luca <deluca1712@gmail.com>
2023-10-27 16:13:13 -0300

    Routes Navlink admin painel

--- src/components/HeaderAdmin/index.tsx
+++ src/components/HeaderAdmin/index.tsx
@@ -2,6 +2,7 @@ import './styles.css';
 import produtcsIcon from '../../assets/products.svg';
 import homeIcon from '../../assets/home.svg';
 import LoggedInUser from '../LoggedInUser';
+import { NavLink } from 'react-router-dom';
 
 
 const HeaderAdmin = () => {
@@ -12,14 +13,22 @@ const HeaderAdmin = () => {
                 <h1>Painel do Administrador</h1>
                 <div className="ec-navbar-header-right">
                     <div className="ec-menu-items-container">
-                        <div className="ec-menu-item">
-                            <img src={homeIcon} alt="Início" />
-                            <p>Início</p>
-                        </div>
-                        <div className="ec-menu-item">
-                            <img src={produtcsIcon} alt="Cadastro de produtos" />
-                            <p className="ec-menu-item-active">Produtos</p>
-                        </div>
+                        <NavLink
+                            to={"/admin/home"}
+                            className={({ isActive }) => isActive ? "ec-menu-item-active" : ""}>
+                            <div className="ec-menu-item">
+                                <img src={homeIcon} alt="Início" />
+                                <p>Início</p>
+                            </div>
+                        </NavLink>
+                        <NavLink
+                            to={"/admin/products"}
+                            className={({ isActive }) => isActive ? "ec-menu-item-active" : ""}>
+                            <div className="ec-menu-item">
+                                <img src={produtcsIcon} alt="Cadastro de produtos" />
+                                <p>Produtos</p>
+                            </div>
+                        </NavLink>
                     </div>
                     <LoggedInUser></LoggedInUser>
                 </div>
