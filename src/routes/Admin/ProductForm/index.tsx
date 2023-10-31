e44bb819479a02392c72e376c7e51d35fa941278
Rafael de Luca <deluca1712@gmail.com>
2023-10-31 14:50:04 -0300

    form insert new product in administrator panel

--- src/routes/Admin/ProductForm/index.tsx
+++ src/routes/Admin/ProductForm/index.tsx
@@ -3,7 +3,41 @@ import "./styles.css";
 const ProductForm = () => {
 
     return (
-        <h1>Formulário de cadastro e edição de produtos</h1>
+        <main>
+            <section id="product-form-section" className="ec-container">
+                <div className="ec-product-form-container">
+                    <form className="ec-card-general ec-form-general">
+                        <h2>Dados do produto</h2>
+                        <div className="ec-form-inputs-container">
+                            <div>
+                                <input className="ec-form-input" type="text" placeholder="Nome"></input>
+                            </div>
+                            <div>
+                                <input className="ec-form-input" type="text" placeholder="Preço"></input>
+                            </div>
+                            <div>
+                                <input className="ec-form-input" type="text" placeholder="Imagem"></input>
+                            </div>
+                            <div>
+                                <select className="ec-form-input ec-select" required>
+                                    <option value="" disabled selected>Categorias</option>
+                                    <option value="1">Valor 1</option>
+                                    <option value="2">Valor 2</option>
+                                </select>
+                            </div>
+                            <div>
+                                <textarea className="ec-form-input ec-textarea" placeholder="Descrição"></textarea>
+                            </div>
+                        </div>
+
+                        <div className="ec-product-form-buttons">
+                            <button type="reset" className="ec-btn ec-btn-cancel">Cancelar</button>
+                            <button type="submit" className="ec-btn ec-btn-salvar">Salvar</button>
+                        </div>
+                    </form>
+                </div>
+            </section>
+        </main>
     );
 }
 
